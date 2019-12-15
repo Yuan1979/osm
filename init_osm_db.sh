5bef67a4525fd62d3a6c2188f53571b048b2f647
yuan <yuangu1979@gmail.com>
2019-12-15 21:10:39 +0100

    osm db init script

---
+++ init_osm_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set +e
+
+echo "start postgis server...."
+docker run --rm --network=host -e POSTGRES_PASSWORD=admin -d -h 127.0.01 -p 5432:5432 mdillon/postgis
+
+echo "downloading osm data from geofabrik..."
+wget https://download.geofabrik.de/europe/luxembourg-latest.osm.pbf
+
+echo "sending data to postgis server"
+export PGPASSWORD=admin
+osm2pgsql -d postgres -C 4096 -U postgres -H 127.0.0.1 -P 5432 /tmp/luxembourg-latest.osm.pbf
+
+set -e
